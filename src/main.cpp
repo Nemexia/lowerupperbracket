11d5ff44e4225ed4e6c08dea930bf47d7a2e625e
Nemexia <nemexia.m@gmail.com>
2026-08-18 02:56:06 +0330

    Add code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include "helper.hpp"
+
 int main()
 {
-
+    std::vector<Player> players;
+    populate(players, 8, 1000.0, 150);
+    print_population_stats(players);
+    auto initial_bracket = make_initial_layer(players);
+    auto next_layer = get_next_layer(initial_bracket);
 }
